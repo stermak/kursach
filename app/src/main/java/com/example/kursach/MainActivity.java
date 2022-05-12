3b4f8ec3b0d1bbae2461a93b6a8b567dc5a39a4d
Ermak <7107798@mail.ru>
2022-05-12 23:40:44 +0300

    12.05 23:40

--- app/src/main/java/com/example/kursach/MainActivity.java
+++ app/src/main/java/com/example/kursach/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
--- app/src/main/java/com/example/kursach/SpisokFragment.java
+++ app/src/main/java/com/example/kursach/SpisokFragment.java
@@ -18,6 +18,7 @@ public class SpisokFragment extends Fragment {
 
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_spisok, container, false);
